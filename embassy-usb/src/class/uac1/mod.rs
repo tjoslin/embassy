3deba9afe9acad899e4d4d25fdd2950b7d4751a9
Todd Joslin <todd.joslin@hellbender.com>
2024-12-03 17:41:03 -0500

    usb-uac1: mic demo

--- embassy-usb/src/class/uac1/mod.rs
+++ embassy-usb/src/class/uac1/mod.rs
@@ -3,6 +3,7 @@
 //! Contains:
 //! - The `speaker` class with a single audio streaming interface (host to device)
 
+pub mod microphone;
 pub mod speaker;
 
 mod class_codes;
@@ -22,6 +23,7 @@ const MAX_AUDIO_CHANNEL_COUNT: usize = MAX_AUDIO_CHANNEL_INDEX + 1;
 #[allow(missing_docs)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum Channel {
+    Mono,
     LeftFront,
     RightFront,
     CenterFront,
@@ -40,6 +42,7 @@ impl Channel {
     /// Map a `Channel` to its corresponding USB Audio `ChannelConfig`.
     fn get_channel_config(&self) -> ChannelConfig {
         match self {
+            Channel::Mono => ChannelConfig::None,
             Channel::LeftFront => ChannelConfig::LeftFront,
             Channel::RightFront => ChannelConfig::RightFront,
             Channel::CenterFront => ChannelConfig::CenterFront,
